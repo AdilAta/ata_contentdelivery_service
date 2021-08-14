4f914330b4800523b507e351df6a133e74968fed
Adil Ata <adil.ata@breakthru.pk>
2021-08-14 14:20:25 -0700

    committing content delivery service

---
+++ ContentDeliveryService/Configs/DownloadSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentDeliveryService.Configs
+{
+    public class DownloadSettings
+    {
+        public string RootPath { get; set; }
+    }
+}
---
+++ ContentDeliveryService/Controllers/DownloadController.cs
@@ -0,0 +1,33 @@
+﻿using ContentDeliveryService.Configs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentDeliveryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : ControllerBase
+    {
+        private DownloadSettings downloadSettings;
+        public DownloadController(IOptions<DownloadSettings> downloadSettings)
+        {
+            this.downloadSettings = downloadSettings.Value;
+        }
+
+        [HttpGet]
+        public FileContentResult DownloadVideo()
+        {
+            //    return File(System.IO.File.ReadAllBytes(Path.Combine(this.downloadSettings.RootPath, "Chapter 1.mp4")), "video/mp4", "MyFirstVideoFile.mp4");
+            return File(System.IO.File.ReadAllBytes(@"./resources/Chapter1.mp4"), "video/mp4", "MyFirstVideoFile.mp4");
+
+        }
+
+
+    }
+}
